cd0c9c2014269e486c8b9ae9be112741da6d2107
Prasetyo Wicaksono <prasetyo@nanomit.es>
2015-01-27 08:57:25 +0700

    refactor application

--- app/Core/Application.php
+++ app/Core/Application.php
@@ -14,7 +14,6 @@ use Doctrine\ORM\Tools\Setup;
 use League\Container\Container;
 use League\Container\ContainerAwareInterface;
 use League\Container\ContainerAwareTrait;
-use League\Container\ContainerInterface;
 use League\Route\RouteCollection;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Logger;
@@ -29,80 +28,110 @@ class Application implements
     use ContainerAwareTrait;
     use EntityManagerAwareTrait;
 
-    private $route;
+    private $routes = [];
 
-    public function init()
+    public function __construct()
     {
+        $this->setContainer(new Container());
+        $this->setEventEmitter(new EventEmitter());
         $this->initService();
-        $this->initEvents();
-        $this->defineDependency();
-        $this->initRoutes($this->getContainer());
+    }
+
+    protected function addRoute($method, $uri, $callback)
+    {
+        if (($callback instanceof \Closure)) {
+            $this->routes[] = [$method, $uri, serialize($callback)];
+        } else {
+            $this->routes[] = [$method, $uri, $callback];
+            list($class_name) = explode("::", $callback);
+            $this->getContainer()->add($class_name)
+                ->withArguments(["EntityManager", "EventEmitter"]);
+        }
+    }
+
+    public function post($uri, $callback)
+    {
+        $this->addRoute("POST", $uri, $callback);
+    }
+
+    public function get($uri, $callback)
+    {
+        $this->addRoute("GET", $uri, $callback);
+    }
+
+    public function put($uri, $callback)
+    {
+        $this->addRoute("PUT", $uri, $callback);
+    }
+
+    public function delete($uri, $callback)
+    {
+        $this->addRoute("DELETE", $uri, $callback);
+    }
+
+    public function registerEvent($listener, $priority = EventEmitter::P_NORMAL)
+    {
+        $this->container->singleton($listener)
+            ->withArguments(["Logger"]);
+        $this->getEventEmitter()->addListener(
+            $listener::getEventName(),
+            function ($event) use ($listener) {
+                 return $this->container->get($listener)->handle($event);
+            },
+            $priority
+        );
+    }
+
+    public function registerService($alias, $callback)
+    {
+        $this->getContainer()->add($alias, $callback);
     }
 
     public function run()
     {
-        $this->getContainer()->get("event")->emit("before.app");
+        $this->getEventEmitter()->setContainer($this->getContainer());
+        $this->getEventEmitter()->emit("before.app");
 
-        // run app here
-        $dispatcher = $this->route->getDispatcher();
+        $router = new RouteCollection($this->getContainer());
 
-        $dispatcher->dispatch("GET", "/")->send();
-
-        $this->getContainer()->get("event")->emit("after.app");
-    }
-
-    protected function initRoutes(ContainerInterface $container)
-    {
-        $route = new RouteCollection($container);
-
-        // define route here
-        $route->addRoute('GET', '/', 'Avent\\Controllers\\IndexController::index');
-        $this->route = $route;
-    }
-
-    protected function initEvents()
-    {
-        $emitter = new EventEmitter();
-
-        $events_dir = APP_PATH . "/Events";
-
-        foreach (glob("{$events_dir}/*Hook.php") as $file) {
-            list($class_name) = explode(".", $file);
-            $class_name = "Avent\\Events\\" . $class_name;
-
-            $listener = new $class_name();
-
-            $emitter->addListener($listener->getEventName(), $listener);
+        foreach ($this->routes as $route) {
+            $router->addRoute($route[0], $route[1], $route[2]);
         }
 
-        $this->setEventEmitter($emitter);
+        $dispatcher = $router->getDispatcher();
 
-        // add event to container
-        $this->container->add("event", $this->getEventEmitter());
+        $this->getEventEmitter()->emit("before.dispatch");
+
+        $response = $dispatcher->dispatch(strtoupper($_SERVER["REQUEST_METHOD"]), $_SERVER["REQUEST_URI"]);
+
+        $this->getEventEmitter()->emit("after.dispatch");
+
+        $response->send();
+
+        $this->getEventEmitter()->emit("after.app");
     }
 
     protected function initService()
     {
-        $container = new Container();
-
         // add config to container
-        $config = (object) include APP_PATH . "/Config/Config.php";
-        $container->add("config", $config);
+        $config = (object)include APP_PATH . "/Config/Config.php";
+
+        define("BASE_URL", $config->application["base_url"]);
 
         // add entity manager to container
-        $container->add("em", function() use ($container) {
+        $this->getContainer()->singleton("EntityManager", function () use ($config) {
             $entity_path = [APP_PATH . "/Entity"];
-            $isDev = $container->get("config")->application["dev"];
-            $db_config = $container->get("config")->database;
+            $isDev = $config->application["dev"];
+            $db_config = $config->database;
 
-            $db_conn =  Setup::createAnnotationMetadataConfiguration($entity_path, $isDev);
+            $db_conn = Setup::createAnnotationMetadataConfiguration($entity_path, $isDev);
 
             return EntityManager::create($db_config, $db_conn);
         });
 
         // add logger to container
-        $container->add("logger", function() use ($container) {
-            $config = $container->get("config")->logger;
+        $this->getContainer()->singleton("Logger", function () use ($config) {
+            $config = $config->logger;
 
             $formatter = new LineFormatter($config["format"], $config["date_format"]);
             $log_level = $config["log_level"];
@@ -116,41 +145,13 @@ class Application implements
             return $logger;
         });
 
-        // add http request to container
-        $request = new Request();
-        $request->createFromGlobals();
-        $container->add("Symfony\\Component\\HttpFoundation\\Request", $request);
+        $this->getContainer()->singleton("Symfony\\Component\\HttpFoundation\\Request", function () {
+            $request = Request::createFromGlobals();
+            $request->overrideGlobals();
+            return $request;
+        });
 
-        // add http response to container
-        $container->add("Symfony\\Component\\HttpFoundation\\Response");
-
-
-        $this->setContainer($container);
-    }
-
-    protected function defineDependency()
-    {
-        $this->injectControllerDependency();
-
-        // define custom class dependency here
-    }
-
-    protected function injectControllerDependency()
-    {
-        $controller_dir = APP_PATH . "/Controllers";
-
-        $files = array_diff(scandir($controller_dir), array('..', '.'));
-
-        foreach ($files as $file) {
-            list($class_name) = explode(".", $file);
-            $class_name = "Avent\\Controllers\\" . $class_name;
-            $this->getContainer()->add($class_name)
-                ->withMethodCall("setConfig", ["config"])
-                ->withMethodCall("setEntityManager", ["em"])
-                ->withMethodCall("setLogger", ["logger"])
-                ->withMethodCall("setEventEmitter", ["event"]);
-        }
-
+        $this->getContainer()->singleton("EventEmitter", $this->getEventEmitter());
     }
 }
 
