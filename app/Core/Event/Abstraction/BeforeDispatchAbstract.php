b9a9f05d764a30f512d10c7d60cba844e2eed99d
Prasetyo Wicaksono <prasetyo@nanomit.es>
2015-01-27 09:02:16 +0700

    add Logger dependency & php doc

--- app/Core/Event/Abstraction/BeforeDispatchAbstract.php
+++ app/Core/Event/Abstraction/BeforeDispatchAbstract.php
@@ -8,17 +8,39 @@ use Avent\Core\Event\EventListenerInterface;
 use Avent\Core\Event\IsListenerTrait;
 use FastRoute\Dispatcher;
 use League\Event\EventInterface;
+use Monolog\Logger;
 
+/**
+ * Class BeforeDispatchAbstract
+ * @package Avent\Core\Event\Abstraction
+ */
 abstract class BeforeDispatchAbstract implements EventListenerInterface
 {
     use IsListenerTrait;
 
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    /**
+     * @param EventInterface $event
+     * @param Dispatcher $dispatcher
+     * @return boolean
+     */
     abstract public function handle(EventInterface $event, Dispatcher $dispatcher = null);
 
     /**
+     * @param Logger $logger
+     */
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+    /**
      * @return string
      */
-    public function getEventName()
+    public static function getEventName()
     {
         return EventEmitter::BEFORE_DISPATCH;
     }
