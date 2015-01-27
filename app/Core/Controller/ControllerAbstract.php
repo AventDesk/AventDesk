3763b5c0f8d1ab4a078661f857b99c4d0eb3d575
Prasetyo Wicaksono <prasetyo@nanomit.es>
2015-01-27 09:03:37 +0700

    remove some dependency

--- app/Core/Controller/ControllerAbstract.php
+++ app/Core/Controller/ControllerAbstract.php
@@ -3,25 +3,23 @@
 
 namespace Avent\Core\Controller;
 
-use Avent\Core\ConfigAwareInterface;
-use Avent\Core\ConfigAwareTrait;
 use Avent\Core\EntityManager\EntityManagerAwareInterface;
 use Avent\Core\EntityManager\EntityManagerAwareTrait;
 use Avent\Core\Event\EventEmitterAwareInterface;
 use Avent\Core\Event\EventEmitterAwareTrait;
-use Avent\Core\Logger\LoggerAwareInterface;
-use Avent\Core\Logger\LoggerAwareTrait;
+use Doctrine\ORM\EntityManagerInterface;
+use League\Event\EmitterInterface;
 
-abstract class ControllerAbstract implements
-    EntityManagerAwareInterface,
-    EventEmitterAwareInterface,
-    ConfigAwareInterface,
-    LoggerAwareInterface
+abstract class ControllerAbstract implements EntityManagerAwareInterface, EventEmitterAwareInterface
 {
     use EntityManagerAwareTrait;
     use EventEmitterAwareTrait;
-    use ConfigAwareTrait;
-    use LoggerAwareTrait;
+
+    public function __construct(EntityManagerInterface $em, EmitterInterface $event)
+    {
+        $this->setEventEmitter($event);
+        $this->setEntityManager($em);
+    }
 }
 
 // EOF
