589a303cc02c41f27110d143c15331d1a499147f
Prasetyo Wicaksono <prasetyo@nanomit.es>
2015-01-28 09:22:18 +0700

    CommandInterface extending RepositoryAware

--- app/Core/CommandBus/CommandInterface.php
+++ app/Core/CommandBus/CommandInterface.php
@@ -3,11 +3,13 @@
 
 namespace Avent\Core\CommandBus;
 
+use Avent\Core\Repository\RepositoryAwareInterface;
+
 /**
  * Interface CommandInterface
  * @package Avent\Core\CommandBus
  */
-interface CommandInterface
+interface CommandInterface extends RepositoryAwareInterface
 {
 
 }
