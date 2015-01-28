bba924e8e68197568c01d46f4f3b2bcb636f3686
Prasetyo Wicaksono <prasetyo@nanomit.es>
2015-01-28 09:26:09 +0700

    fix doctrine cli config

--- cli-config.php
+++ cli-config.php
@@ -1,9 +1,10 @@
 <?php
 
+define("APP_PATH", __DIR__ . "/app");
+
 $app = new \Avent\Core\Application();
-$app->init();
 
 $di = $app->getContainer();
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($di->get("em"));
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($di->get("EntityManager"));
 
 // EOF
