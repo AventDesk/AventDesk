7563ea7ea97b5b79933e43d5ba047ca4e5ad7998
Prasetyo Wicaksono <jowy@oiry.net>
2015-02-03 21:14:20 +0700

    add docs and method

--- app/Repository/PersonRepository.php
+++ app/Repository/PersonRepository.php
@@ -6,12 +6,28 @@ namespace Avent\Repository;
 use Avent\Entity\Person;
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class PersonRepository
+ * @package Avent\Repository
+ */
 class PersonRepository extends EntityRepository
 {
+    /**
+     * @param Person $person
+     */
     public function save(Person $person)
     {
         $this->_em->persist($person);
         $this->_em->flush();
+    }
+
+    /**
+     * @param $person_id
+     * @return Person|null
+     */
+    public function findOneByPersonId($person_id)
+    {
+        return $this->findOneBy(["person_id" => (int) $person_id]);
     }
 }
 
