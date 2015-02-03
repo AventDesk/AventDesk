fe757238d44c22e63823ede156e3cae34ff6e9af
Prasetyo Wicaksono <jowy@oiry.net>
2015-02-03 21:11:33 +0700

    refactor rename method

--- app/CommandBus/Handler/UserRegistrationHandler.php
+++ app/CommandBus/Handler/UserRegistrationHandler.php
@@ -7,23 +7,23 @@ namespace Avent\CommandBus\Handler;
 use Avent\Core\CommandBus\CommandInterface;
 use Avent\Core\CommandBus\HandlerInterface;
 use Avent\Core\Services\ServicesFactoryAwareTrait;
-use Avent\Services\DomainServiceFactory;
-use Avent\Services\InfrastructureServiceFactory;
 
+/**
+ * Class UserRegistrationHandler
+ * @package Avent\CommandBus\Handler
+ */
 class UserRegistrationHandler implements HandlerInterface
 {
     use ServicesFactoryAwareTrait;
 
-    public function __construct(DomainServiceFactory $domain_services, InfrastructureServiceFactory $infra_services)
-    {
-        $this->setDomainServicesFactory($domain_services);
-        $this->setInfrastructureServicesFactory($infra_services);
-    }
-
+    /**
+     * @param CommandInterface $command
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function handle(CommandInterface $command)
     {
         $registration_service = $this->getDomainServicesFactory()->createUserRegistrationService();
-        return $registration_service->register($command);
+        return $registration_service->execute($command);
     }
 }
 
