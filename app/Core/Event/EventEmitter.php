7c6fde5193666c96daaafb19755e768a33114fb3
Prasetyo Wicaksono <jowy@oiry.net>
2015-02-06 13:59:24 +0700

    lazy event emitter

--- app/Core/Event/EventEmitter.php
+++ app/Core/Event/EventEmitter.php
@@ -4,22 +4,59 @@
 namespace Avent\Core\Event;
 
 use Avent\Core\Event\Exception\InvalidEventNameException;
+use League\Container\ContainerAwareInterface;
 use League\Container\ContainerAwareTrait;
 use League\Event\Emitter;
-use League\Container\ContainerAwareInterface;
+use League\Event\EventInterface;
 
+/**
+ * Class EventEmitter
+ * @package Avent\Core\Event
+ */
 class EventEmitter extends Emitter implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
+    /**
+     * @var string
+     */
     const BEFORE_APP = "before.app";
+    /**
+     * @var string
+     */
     const AFTER_APP = "after.app";
+    /**
+     * @var string
+     */
     const BEFORE_DISPATCH = "before.dispatch";
+    /**
+     * @var string
+     */
     const AFTER_DISPATCH = "after.dispatch";
+    /**
+     * @var string
+     */
     const ON_ERROR = "on.error";
+    /**
+     * @var string
+     */
     const BEFORE_USER_REGISTRATION = "before.user.Registration";
+    /**
+     * @var string
+     */
     const AFTER_USER_REGISTRATION = "after.user.registration";
+    /**
+     * @var string
+     */
+    const SECURITY_EVENT = "security.event";
 
+    /**
+     * @param string $event
+     * @param string $listener
+     * @param int $priority
+     * @return $this
+     * @throws InvalidEventNameException
+     */
     public function addListener($event, $listener, $priority = self::P_NORMAL)
     {
         $class_reflection = new \ReflectionClass("Avent\\Core\\Event\\EventEmitter");
@@ -29,7 +66,46 @@ class EventEmitter extends Emitter implements ContainerAwareInterface
             throw new InvalidEventNameException("Event name {$event} is not valid");
         }
 
-        return parent::addListener($event, $listener, $priority);
+        if (! isset($this->listeners[$event])) {
+            $this->listeners[$event] = [];
+        }
+
+        $this->listeners[$event][] = [$listener, $priority];
+        $this->clearSortedListeners($event);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function emit($event)
+    {
+        list($name, $event) = $this->prepareEvent($event);
+        $arguments = [$event] + func_get_args();
+
+        $this->invokeListeners($name, $event, $arguments);
+        $this->invokeListeners("*", $event, $arguments);
+
+        return $event;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function invokeListeners($name, EventInterface $event, array $arguments)
+    {
+        $listeners = $this->getListeners($name);
+
+        foreach ($listeners as $listener) {
+            if ($event->isPropagationStopped()) {
+                break;
+            }
+
+            $listener = $this->getContainer()->get($listener);
+
+            $this->ensureListener($listener);
+
+            call_user_func_array([$listener, "handle"], $arguments);
+        }
     }
 }
 
